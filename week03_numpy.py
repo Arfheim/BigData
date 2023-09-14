ffc032d01a98c19e260f46775ad2b34a413ee3ed
Arfheim <h680h620@naver.com>
2023-09-14 15:32:42 +0900

    week03 v1.3 2d array

--- week03_numpy.py
+++ week03_numpy.py
@@ -4,8 +4,7 @@ import tkinter as tk #Built in GUI
 from tkinter import messagebox #popup
 def click_button():
     try:
-        r = int(en_row.get())
-        c = int(en_column.get())
+        r,c = map(int,en_row_column.get().split())
         rows = [[random.randint(1, 100) for i in range(r)] for i in range(c)]
 
         matrix = np.array(rows, dtype='int16')
@@ -15,21 +14,22 @@ def click_button():
         messagebox.showerror('Error!',f"입력된 값이 없습니다.\n{err}")
 
 window = tk.Tk()
-window.title('numpy gui version v1.2')
+window.title('numpy gui version v1.3')
 window.geometry('300x150')
 #create widget
-lbl_result = tk.Label(text="random numpy array")
-en_row = tk.Entry()
-en_column = tk.Entry()
+lbl_result = tk.Label(text="random numpy 2d array")
+en_row_column = tk.Entry()
 btn_click = tk.Button(text="click me", command=click_button)
 #widget layout
 # lbl_result.place(x=50,y=50)
 # btn_click.place(x=0,y=0)
-lbl_result.grid(row=0, column=0, columnspan=2)
-en_row.grid(row=1, column=0)
-en_column.grid(row=1, column=1)
-btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
-
+# lbl_result.grid(row=0, column=0, columnspan=2)
+# en_row.grid(row=1, column=0)
+# en_column.grid(row=1, column=1)
+# btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
+lbl_result.pack()
+en_row_column.pack(fill='x')
+btn_click.pack(fill='x')
 
 window.mainloop()
 
