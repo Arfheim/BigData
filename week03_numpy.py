a5f6a6e578e2cc3cebafac2f2ec64f7cf7c376cb
Arfheim <h680h620@naver.com>
2023-09-14 14:37:29 +0900

    week03 v0.8 exception handling

---
+++ week03_numpy.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+import tkinter as tk #Built in GUI
+
+def click_button():
+    try:
+        a = int(en_number.get())
+        lst = [random.randint(1, 100) for i in range(a)]
+        v = np.array(lst, dtype='int16')
+        lbl_result.config(text=v)
+    except ValueError as err :
+        lbl_result.config(text=f"입력된 값이 없습니다.\n{err}")
+
+window = tk.Tk()
+window.title('numpy gui version v0.8')
+window.geometry('300x150')
+#create widget
+lbl_result = tk.Label(text="random numpy array")
+en_number = tk.Entry()
+btn_click = tk.Button(text="click me", command=click_button)
+#widget layout
+lbl_result.pack()
+en_number.pack(fill='x')
+btn_click.pack(fill='x')
+
+
+window.mainloop()
+
