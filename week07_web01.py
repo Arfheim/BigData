95121ecb5339a7b56669b63aeedce058d9aec062
Arfheim <h680h620@naver.com>
2023-10-12 14:43:59 +0900

    week07 v3.1 web

--- week07_web01.py
+++ week07_web01.py
@@ -1,7 +1,17 @@
+# import urllib.request
+#
+# url = "https://www.daelim.ac.kr/type/KOR_A/img/intro/logo.png"
+#
+# urllib.request.urlretrieve(url, "daelim.png")  #웹에서 가져온 이미지를 보조기억장치에 저장
+#
+# print("저장완료")
+
 import urllib.request
 
 url = "https://www.daelim.ac.kr/type/KOR_A/img/intro/logo.png"
 
-urllib.request.urlretrieve(url, "daelim.png")
+logo = urllib.request.urlopen(url).read() #이미지를 다운받아서 메모리에 저장
 
-print("저장완료")
+with open("daelim.png", "wb") as file: #원본 이미지가 png 포멧이므로 쓰기모드를 텍스트가 아닌 바이너리 형식으로 설정
+    file.write(logo)
+    print("저장완료")
