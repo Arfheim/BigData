3abe1a94c40c3f087d51d5de5f1f2c4c326a03cf
Arfheim <h680h620@naver.com>
2023-10-12 16:06:12 +0900

    week07 v3.5 web

---
+++ week07_web04.py
@@ -0,0 +1,15 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+api = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
+urls = urllib.request.urlopen(api).read()
+soup = BeautifulSoup(urls,"html.parser")
+
+cities = soup.find_all("city")
+wfs = soup.find_all("wf")
+wfs.pop(0) # 성능 이슈 있을 수 있음
+
+for i in range(len(cities)):
+    print(f'{cities[i].string}의 날씨는 {wfs[i*13].string}입니다.')
+
+#print(len(cities), len(wfs))
