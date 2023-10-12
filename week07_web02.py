20b69034f3e94e6e024b95b5c18bf2039381f841
Arfheim <h680h620@naver.com>
2023-10-12 14:51:20 +0900

    week07 v3.2 web

---
+++ week07_web02.py
@@ -0,0 +1,16 @@
+import urllib.request
+import urllib.parse
+
+api = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
+
+station_id = input("지역코드 : ") #전국 108, 수도권 109, 강원 105, 제주 184
+
+values = {"stnId" : station_id}
+parameters = urllib.parse.urlencode(values)
+url = api + "?" + parameters # api string + ? + stnId=station_id
+
+print(url)
+
+urls = urllib.request.urlopen(url).read()
+texts = urls.decode("utf-8")
+print(texts)
